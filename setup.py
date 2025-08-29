e5174b53ca710bca59b64079e01bf30bff1df5b3
Alain Muls <alain.muls@gmail.com>
2025-08-29 11:26:06 +0200

    adjusted setup.py after modif of pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 from setuptools import setup, find_packages
-
-setup(
-    name="gnss_predict",
-    version="1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={
-        "gnss_predict": ["GNSS/*", "tle/*", "utils/*"]
-    },
-    include_package_data=True,
-    install_requires=[
-        "ephem",
-        "numpy",
-        "matplotlib",
-        "cartopy",
-        "rich",
-        "pandas",
-    ],
-    entry_points={
-        "console_scripts": [
-            "gnss_predict=gnss_predict.main:main",
-        ],
-    },
-)
+ 
+# This file is needed for editable installs (`pip install -e .`)
+# and for backward compatibility with older versions of pip.
+setup()
