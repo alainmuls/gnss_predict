d69d0041fa749140d969220536714df8be0668a8
Alain Muls <alain.muls@gmail.com>
2026-03-17 18:56:48 +0100

    switching to UV from pip

--- setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_packages
- 
-# This file is needed for editable installs (`pip install -e .`)
-# and for backward compatibility with older versions of pip.
-setup()
